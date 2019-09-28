2bc0701b53b779d705b0d0d0c1cdd4fdd5bd87d3
harrychengly <harrychengly@gmail.com>
2019-09-28 12:34:05 +0800

    wedding-nightmare v0.1

--- controllers/WeddingNightmareController.js
+++ controllers/WeddingNightmareController.js
@@ -1,9 +1,60 @@
-var WeddingNightmareController = function(app) {
-    app.post("/wedding-nightmare", function(req, res) {
+var WeddingNightmareController = function (app) {
+    app.post("/wedding-nightmare", function (req, res) {
         // POST request
         // ...
+        var json = req.body;
+        // console.log(json)
+        // console.log("hello")
         try {
-            res.status(200).json({ message : 'Success'});
+            outputArr = []
+
+            for (i = 0; i < json.length; i++) {
+                // console.log("here")
+                // console.log(json[i])
+                var testCase = json[i]
+                console.log(i)
+                var output = {
+                    test_case: testCase.test_case,
+                    satisfiable: "",
+                    allocation: []
+                }
+
+                var satisfiable = false
+                var guestNum = testCase.guests
+                var tableNum = testCase.tables
+                var enemies = testCase.enemies
+                var allocation = []
+                var guestStack = []
+                for (j = guestNum; j > 0; j--) {
+                    guestStack.push(j)
+                }
+
+                // console.log(guestStack)
+                while (guestStack.length != 0) {
+                    if (enemies.length == 0) {
+                        allocation.push([guestStack.pop(), tableNum - tableNum + 1])
+                    } else {
+                        guestStack.pop()
+                    }
+                }
+
+                if (allocation.length == 0) {
+                    satisfiable = false
+                } else {
+
+                    satisfiable = true
+                }
+                output["satisfiable"] = satisfiable
+                output["allocation"] = allocation
+                // console.log(allocation)
+                // console.log(output)
+                outputArr.push(output)
+                // console.log(JSON.stringify(outputArr))
+            }
+            console.log(outputArr)
+            res.status(200).json(
+                outputArr
+            );
 
         } catch (err) {
             res.send(err.message);
