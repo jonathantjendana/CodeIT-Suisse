087e4499315c9082e10a4038f940ec073009ac46
Jonathan Tjendana <jonathan.tjendana@gmail.com>
2019-09-28 12:09:50 +0800

    chessgame v0.2

--- controllers/WeddingNightmareController.js
+++ controllers/WeddingNightmareController.js
@@ -21,100 +21,146 @@ var WeddingNightmareController = function (app) {
                     }
                 }
             }
-            var count = 0;
-            for (var i = 0; i < n; i++) {
-                //x is constant
-                var x = queen["i"]
-                var test = json[x][i]
-                if (test == "") {
-                    count++;
-                }
-
-                if (test == "X") {
-                    break;
-                }
-            }
-
-            for (var i = 0; i < n; i++) {
-                //y is constant
-                var y = queen["j"]
-                var test = json[i][y]
-                if (test == "") {
-                    count++;
-                }
-
-                if (test == "X") {
-                    break;
-                }
-            }
-
             var x = queen["i"]
             var y = queen["j"]
-            for (var i = 0; i < n; i++) {
-                var j = x - i;
-                var k = y - i;
-                try {
-                    var test = json[j][k]
-                    console.log(test)
-                    if (test == "X") {
-                        break;
-                    } else if (test == "") {
-                        console.log(test)
-                        count++;
-                    }
-
-                } catch (ex) {
-
-                }
-            }
-            for (var i = 0; i < n; i++) {
-                var j = x + i;
-                var k = y + i;
-                try {
-                    var test = json[j][k]
-                    if (test == "X") {
-                        break;
-                    } else if (test == "") {
-                        count++;
-                    }
-                } catch (ex) {
-
-                }
-            }
-
-            for (var i = 0; i < n; i++) {
-                var j = x - i;
-                var k = y + i;
-                try {
-                    var test = json[j][k]
-                    if (test == "X") {
-                        break;
-                    } else if (test == "") {
-                        count++;
-                    }
-                } catch (ex) {
-
-                }
-            }
-
-            for (var i = 0; i < n; i++) {
-                var j = x + i;
-                var k = y - i;
-                try {
-                    var test = json[j][k]
-                    if (test == "X") {
-                        break;
-                    } else if (test == "") {
-                        count++;
-                    }
-                } catch (ex) {
-
-                }
-            }
-
             
+            var count = 0;
+            for (var i = 1; i < n-1; i++) {
+                //x is constant
+                var j = x;
+                var k = y - i;
+                try {
+                    var test = json[j][k]
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        count++;
+                    }
 
+                } catch (ex) {
+                    break;
+                }
+            }
+            console.log(count)
+            for (var i = 1; i < n-1; i++) {
+                //x is constant
+                var j = x;
+                var k = y + i;
+                try {
+                    var test = json[j][k]
 
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        count++;
+                    }
+
+                } catch (ex) {
+
+                }
+            }
+            console.log(count)
+            for (var i = 1; i < n-1; i++) {
+                //y is constant
+                var j = x - i;
+                var k = y;
+                try {
+                    var test = json[j][k]
+
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        count++;
+                    }
+
+                } catch (ex) {
+
+                }
+            }
+            console.log(count)
+            for (var i = 1; i < n-1; i++) {
+                //y is constant
+                var j = x + i;
+                var k = y;
+                try {
+                    var test = json[j][k]
+
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        count++;
+                    }
+
+                } catch (ex) {
+
+                }
+            }
+            console.log(count)
+
+            for (var i = 1; i < n-1; i++) {
+                var j = x - i;
+                var k = y - i;
+                try {
+                    var test = json[j][k]
+
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        count++;
+                    }
+
+                } catch (ex) {
+
+                }
+            }
+            console.log("top left" +count)
+            for (var i = 1; i < n-1; i++) {
+                var j = x + i;
+                var k = y + i;
+                try {
+                    var test = json[j][k]
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        count++;
+                    }
+                } catch (ex) {
+
+                }
+            }
+            console.log("right bottom" +count)
+            for (var i = 1; i < n-1; i++) {
+                var j = x - i;
+                var k = y + i;
+                try {
+                    var test = json[j][k]
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        console.log(j)
+                        console.log(k)
+                        count++;
+                    }
+                } catch (ex) {
+
+                }
+            }
+            console.log("top right" +count)
+            for (var i = 1; i < n-1; i++) {
+                var j = x + i;
+                var k = y - i;
+                try {
+                    var test = json[j][k]
+                    if (test == "X") {
+                        break;
+                    } else if (test == "") {
+                        
+                        count++;
+                    }
+                } catch (ex) {
+
+                }
+            }
 
             console.log(count);
 
